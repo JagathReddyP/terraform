5c171119610c7a749b6d89828a3f5a85731e7f34
JagathReddy <jagathsaipj81@gmail.com>
2026-04-07 02:04:36 +0530

    terraform

--- variables/ec2.tf
+++ variables/ec2.tf
@@ -1,4 +1,6 @@
 resource "aws_instance" "terraform" {
+    ami = var.ami_id # left side and right side names need not to be same
+    instance_type = var.instance_type
     vpc_security_group_ids = [aws_security_group.allow_ssh_terrafrom.id]
     tags = var.tags
 }
